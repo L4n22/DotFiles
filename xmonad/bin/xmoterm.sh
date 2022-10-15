8a80c1c9c79bb2192f8246c38f71f1fd1e0e2728
L4n22 <89106058+L4n22@users.noreply.github.com>
2022-10-15 12:22:23 +0900

    Update xmoterm.sh

--- xmonad/bin/xmoterm.sh
+++ xmonad/bin/xmoterm.sh
@@ -11,8 +11,6 @@ elif [ "$1" == "--full" ]; then
         alacritty --class 'Fullscreen,Fullscreen' --config-file "$CONFIG" \
                           -o window.startup_mode=fullscreen window.padding.x=30 window.padding.y=>
                           window.opacity=0.95 font.size=14
-elif [ "$1" == "--current" ]; then
-        alacritty --working-directory "$(pwd)" --config-file "$CONFIG"
 
 else
         alacritty --config-file "$CONFIG"
