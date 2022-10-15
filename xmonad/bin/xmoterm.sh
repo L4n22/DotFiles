ffe780ed6b2c4aab3439ea1bb8db8091e40ea2fe
L4n22 <89106058+L4n22@users.noreply.github.com>
2022-10-15 12:09:17 +0900

    Update xmoterm.sh

--- xmonad/bin/xmoterm.sh
+++ xmonad/bin/xmoterm.sh
@@ -6,11 +6,14 @@
 CONFIG="$HOME/.config/alacritty/alacritty.yml"
 
 if [ "$1" == "--float" ]; then
-	alacritty --class 'alacritty-float,alacritty-float' --config-file "$CONFIG"
+        alacritty --class 'alacritty-float,alacritty-float' --config-file "$CONFIG"
 elif [ "$1" == "--full" ]; then
-	alacritty --class 'Fullscreen,Fullscreen' --config-file "$CONFIG" \
-			  -o window.startup_mode=fullscreen window.padding.x=30 window.padding.y=30 \
-			  window.opacity=0.95 font.size=14
+        alacritty --class 'Fullscreen,Fullscreen' --config-file "$CONFIG" \
+                          -o window.startup_mode=fullscreen window.padding.x=30 window.padding.y=>
+                          window.opacity=0.95 font.size=14
+elif [ "$1" == "--current" ]; then
+        alacritty --working-directory "$(pwd)" --config-file "$CONFIG"
+
 else
-	alacritty --config-file "$CONFIG"
+        alacritty --config-file "$CONFIG"
 fi
