5eb3714cee1b3a6f40a1cb55bd1f0b8719c323e3
L4n22 <89106058+L4n22@users.noreply.github.com>
2023-12-19 20:39:33 -0800

    Update autostart.sh

--- xmonad/bin/autostart.sh
+++ xmonad/bin/autostart.sh
@@ -11,20 +11,20 @@ for _prs in "${_ps[@]}"; do
 done
 
 #xrandr --output Virtual1 --mode 1920x1080
-sxhkd &
-flameshot &
+#sxhkd &
+#flameshot &
 wmname LG3D &
 #/usr/bin/vmware-user & #Remove this line if you are using virtualbox
-/usr/bin/VBoxClient --clipboard &  #add this line if you are using virtualbox version 6.1.38
+#/usr/bin/VBoxClient --clipboard &  #add this line if you are using virtualbox version 6.1.38
 #/usr/bin/VBoxClient --vmsvga & #add this line if you are using virtualbox version 6.1.38
 # Fix cursor
-xsetroot -cursor_name left_ptr
+#xsetroot -cursor_name left_ptr
 
 # Polkit agent
-/usr/lib/xfce-polkit/xfce-polkit &
+#/usr/lib/xfce-polkit/xfce-polkit &
 
 # Enable power management
-xfce4-power-manager &
+# xfce4-power-manager &
 
 # Enable Super Keys For Menu
 #ksuperkey -e 'Super_L=Alt_L|F1' &
@@ -32,7 +32,7 @@ xfce4-power-manager &
 
 # Restore wallpaper
 #hsetroot -cover /usr/share/backgrounds/nordic-wallpapers-git/sola.png
-hsetroot -cover /usr/share/backgrounds/kali-16x9/kali-red-sticker.jpg 
+#hsetroot -cover /usr/share/backgrounds/kali-16x9/kali-red-sticker.jpg 
 #hsetroot -cover ~/.xmonad/wall/a3.png
 #hsetroot -cover ~/.xmonad/wall/gruvbox_a1.png
 
@@ -45,7 +45,7 @@ hsetroot -cover /usr/share/backgrounds/kali-16x9/kali-red-sticker.jpg
 # Lauch compositor
 ~/.xmonad/bin/xmocomp.sh
 
-# Start mpd
-exec mpd &
+# Start mpd music
+# exec mpd & 
 
 picom --config ~/.xmonad/picom.conf &
